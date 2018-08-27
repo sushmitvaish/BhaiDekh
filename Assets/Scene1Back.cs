6a14784af5feeaca0454f4afdd4e830debede9f4
Vansh Arora <vansha10@gmail.com>
2018-08-27 20:06:38 +0530

    First commit

---
+++ Assets/Scene1Back.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Scene1Back : MonoBehaviour {
+
+	// Use this for initialization
+	public void ButtonClick()
+	{
+		SceneManager.LoadScene ("Scene1");
+	}
+}
---
+++ Assets/linearClick.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class linearClick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void Update () {
+		if (Input.GetMouseButtonDown(0)){ // if left button pressed...
+			Ray ray =Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit)){
+				if(hit.collider.name == "Linear")
+				{
+					SceneManager.LoadScene ("Scene4");
+				}
+			}
+		}
+	}
+}
