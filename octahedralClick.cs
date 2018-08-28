906f69153b2e59d577aea9087391464f1074aeda
sushmitvaish <35952453+sushmitvaish@users.noreply.github.com>
2018-08-28 10:15:48 +0530

    Add files via upload

---
+++ octahedralClick.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class octahedralClick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void Update () {
+		if (Input.GetMouseButtonDown(0)){ // if left button pressed...
+			Ray ray =Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit)){
+				if(hit.collider.name == "Octahedral")
+				{
+					SceneManager.LoadScene ("Scene2");
+				}
+			}
+		}
+	}
+}
---
+++ rotate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+
+	float rotSpeed = 60;
+
+	void OnMouseDrag(){
+		float rotX = Input.GetAxis ("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+		float rotY = Input.GetAxis ("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
+		float rotZ = Input.GetAxis ("Mouse Z") * rotSpeed * Mathf.Deg2Rad;
+		transform.Rotate (Vector3.up, -rotX);
+		transform.Rotate (Vector3.right, rotY);
+		transform.Rotate (Vector3.up, rotZ);
+	}
+}
